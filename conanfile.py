5ebf7e80f7f92df82cba96fbdf6b1a24635792ab
Arenoros <arenoros@gmail.com>
2020-10-10 02:31:01 +0300

    add win32 build

--- conanfile.py
+++ conanfile.py
@@ -2,31 +2,70 @@ import os, re
 import platform
 from pathlib import Path
 from conans import ConanFile, CMake, tools, AutoToolsBuildEnvironment
+import json
 
 class NginxConfig():
-    def __init__(self, cc, prefix):
-        self.cflags = []
-        self.ldflags = []
-        self.cc = cc
+    def __init__(self, cc, prefix, cross):
+        self._cflags = []
+        self._ldflags = []
+        self._cc = cc
+        self._cross = cross
         self.prefix = prefix
-        self.modules = []
+        self._modules = []
+        self._d_modules = []
+        self.cc_args = ''
 
-    def add_mod(self, mod):
+    def add_mod(self, *mods):
         for mod in mods:
-            self.modules.append(f'--with-{mod}')
-        #self.modules.append(f'--with-{mod}')
+            self._modules.append(f'--with-{mod}')
     
     def disable_mod(self, *mods):
         for mod in mods:
-            self.modules.append(f'--without-{mod}')
-        #self.modules.append(f'--without-{mod}')
+            self._modules.append(f'--without-{mod}')
 
     def custom_mod(self, *argv):
         for path in argv:
-            self.modules.append(f'--add-module={path}')
+            self._modules.append(f'--add-module={path}')
 
-    def cflag(self, *opts):
-        self.cflags += opts
+    def add_cflags(self, *opts):
+        self._cflags += opts
+    
+    def add_ldflags(self, *opts):
+        self._ldflags += opts
+
+    def set_args(self, args):
+        link = re.compile(r'^(-l|-L|-link|/link|-LIBPATH|.*\.lib|.*\.so|.*\.a$)')
+        
+        for arg in args:
+            if link.search(arg):
+                self.add_ldflags(arg)
+            else:
+                self.add_cflags(arg)
+        
+
+    @property
+    def cflags(self):
+        return '--with-cc-opt="' + ' '.join(self._cflags) + '"'
+
+    @property
+    def ldflags(self):
+        return f'--with-ld-opt="{" ".join(self._ldflags)}"'
+
+    @property
+    def modules(self):
+        return ' '.join(self._modules)
+
+    @property
+    def cross(self):
+        return f'--crossbuild={self._cross}'
+
+    @property
+    def cc(self):
+        return f'--with-cc={self._cc}'
+
+    def cmd(self):
+        return ' '.join([f'--conan --prefix={self.prefix}', self.cross, self.cc, self.modules, self.cflags, self.ldflags])
+
 
 class NginxConan(ConanFile):
     name = "nginx"
@@ -37,66 +76,28 @@ class NginxConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     options = {
         "shared": [True, False],
-        "with_pcre": [True, False],
         "with_ssl": [True, False]
     }
     default_options = {
         "shared": False,
-        "with_pcre": True,
         "with_ssl": True
     }
     generators = "compiler_args"
     _source_dir = 'src_dir'
     _prefix = 'out'
-
-    requires = "zlib/1.2.11", "openssl/1.1.1f"
+    exports_sources = ["0001-fix-win32-build.patch", "Makefile"]
+    requires = "zlib/1.2.11", "openssl/1.1.1f", 'pcre/[>=8.41]'
 
     def build_requirements(self):
         if tools.os_info.is_windows and not tools.get_env("CONAN_BASH_PATH"):
             self.build_requires("msys2/20200517")
-        # if platform.system() == "Windows":
-        #     #self.build_requires("mingw_installer/1.0@conan/stable")
-        #     self.build_requires("msys2/20190524")
-            pass
 
     def requirements(self):
-        if self.options.with_pcre:
-            self.requires.add("pcre/[>=8.41]")
         if self.settings.os == 'Android' and int(f'{self.settings.os.api_level}') < 28:
             self.requires.add("ndk-libc-fix/[>=0.2]")
 
     def linux_build(self):
-        lib_re = re.compile(r'(^-l|^-L)')
-        cc_re = re.compile(r'(^-Wl,-rpath=|^-L|^-l)') #|^-std=|^-stdlib=
-        params = self.all_flags.split()
-
-        libs = ' '.join([flag for flag in params if lib_re.match(flag)])
-        if tools.get_env("LDFLAGS"):
-            libs = tools.get_env("LDFLAGS") + ' ' + libs
-
-
-        cc_flags = ' '.join([flag for flag in params if not cc_re.match(flag)])
-        if tools.get_env("CFLAGS"):
-            cc_flags = tools.get_env("CFLAGS") + ' ' + cc_flags
-
-        out_dir = Path(self._prefix).resolve()
-        prefix = f'--prefix={out_dir}'
-        crossbuild = f'--crossbuild={self.settings.os}::{self.settings.arch}'
-
-        cc = '--with-cc=$CC' if tools.get_env("CC") else ''
-
-        modules = ['--with-http_v2_module', '--with-http_ssl_module']
-        if self.options.with_pcre:
-            modules.append('--with-pcre')
-            custom_modules = '--add-module=modules/nginx-let-module'
-        else:
-            modules.append('--without-http_rewrite_module')
-        if self.settings.os == 'Linux':
-            cc_flags += ' -pthread'
-        
-        cc_flags=f'--with-cc-opt="{cc_flags}"'
-        ld_flags=f'--with-ld-opt="{libs}"'
-        cmd = ' '.join([prefix, crossbuild, cc, custom_modules, ' '.join(modules), cc_flags, ld_flags])
+        cmd = self.ngx.cmd()
         self.output.info(cmd)
         with tools.chdir(self._source_dir):
             self.run(f'./auto/configure {cmd}')
@@ -104,11 +105,16 @@ class NginxConan(ConanFile):
             self.run('make install')
 
     def win_build(self):
+        cmd = self.ngx.cmd()
+        self.output.info(cmd)
         env_build = AutoToolsBuildEnvironment(self, win_bash=True)
-        with tools.environment_append(env_build.vars):
-            self.run('pwd')
-            with tools.chdir(self._source_dir):
-                self.run(f'./auto/configure --with-cc=cl --builddir=objs --prefix=../out', win_bash=True)
+        with tools.vcvars(self.settings):
+            with tools.environment_append(env_build.vars):
+                with tools.chdir(self._source_dir):
+                    self.run(f'./auto/configure {cmd}', win_bash=True)
+                    self.output.info(Path.cwd())
+                    self.run(f'nmake')
+                    self.run('nmake install')
         pass
 
     def configure(self):
@@ -119,20 +125,17 @@ class NginxConan(ConanFile):
         if not cc and platform.system() == 'Windows':
             cc = 'cl'
 
-        self.ngx = NginxConfig(cc, self._prefix)
-        
+        self.ngx = NginxConfig(cc, self._prefix, f'{self.settings.os}::{self.settings.arch}')
+        #self.ngx.add_mod('threads')
+        self.ngx.add_mod('file-aio')
+        self.ngx.add_mod('http_gunzip_module')
+        self.ngx.custom_mod('modules/nginx-let-module')
         if self.options.with_ssl:
             self.ngx.add_mod('http_v2_module')
             self.ngx.add_mod('http_ssl_module')
 
-        if self.options.with_pcre:
-            self.ngx.add_mod('pcre')
-            self.ngx.custom_mod('modules/nginx-let-module')
-        else:
-            self.ngx.disable_mod('http_rewrite_module')
-
         if self.settings.os == 'Linux':
-            cc_flags += ' -pthread'
+            self.ngx.add_cflags('-pthread')
 
     def source(self):
         git = tools.Git(folder=self._source_dir)
@@ -140,15 +143,16 @@ class NginxConan(ConanFile):
 
     def build(self):
         self.output.info(os.getcwd())
-        self.all_flags = tools.load("conanbuildinfo.args")
+
+        args = tools.load("conanbuildinfo.args").replace('\\', '/').split()
+
+        self.ngx.set_args(args)
         if platform.system() != 'Windows':
             self.linux_build()
         else:
             self.win_build()
 
     def package(self):
-        self.copy("*", src=self._prefix, dst="nginx", keep_path=True)
+        out_dir = Path(self._source_dir) / self._prefix
+        self.copy("*", src=out_dir, dst="nginx", keep_path=True)
     
-
-    # def package_info(self):
-    #     self.env_info.PATH.append(os.path.join(self.package_folder, "bin"))
